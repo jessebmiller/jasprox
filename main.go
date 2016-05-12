170687b2200718392a70466e43418add811ddc6b
Jesse B. Miller <jesse@jessebmiller.com>
2016-05-12 04:23:41 +0000

    slowly decoupling

--- main.go
+++ main.go
@@ -1,3 +1,4 @@
+// Just a simple reverse proxy.
 package main
 
 import (
@@ -6,29 +7,47 @@ import (
 	"net/http"
 	"net/http/httputil"
 	"net/url"
+	"os"
 )
 
-func fixedJbmMicroBackend(r *http.Request) *url.URL {
-	switch r.Host {
-	case "general.show":
-		r.URL.Host = "localhost:8000"
-	case "jessebmiller.com":
-		r.URL.Host = "localhost:8001"
-	default:
-		r.URL.Host = "localhost:8001"
+
+const USE_STRAT string = "environ"
+var STRATS = map[string]func(*http.Request) (*url.URL, error){
+
+	"environ": environStrategy,
+
+	"justJesse": func(r *http.Request) (*url.URL, error) {
+		return url.Parse("http://jessebmiller.com:8000")
+	},
+}
+
+
+func environStrategy(r *http.Request) (*url.URL, error) {
+	return url.Parse(os.Getenv("PROXY_URL"))
+}
+
+
+// whichUrl chooses which url the request should proxy to, or returns an error
+func whichUrl(r *http.Request) (*url.URL, error) {
+	return STRATS[USE_STRAT](r)
+}
+
+
+// serveProxy chooses a url then proxies the request, or responds not found.
+func serveProxy(w http.ResponseWriter, r *http.Request) {
+	proxyUrl, err := whichUrl(r)
+	if err != nil {
+		fmt.Println(err)
+		http.NotFound(w, r)
+	} else {
+		proxy := httputil.NewSingleHostReverseProxy(proxyUrl)
+		proxy.ServeHTTP(w, r)
 	}
-
-	return r.URL
 }
 
-func jasHandler(w http.ResponseWriter, r *http.Request) {
-	proxyURL := fixedJbmMicroBackend(r)
-	proxy := httputil.NewSingleHostReverseProxy(proxyURL)
-	proxy.ServeHTTP(w, r)
-	fmt.Println(r.URL, proxyURL)
-}
 
 func main() {
 	fmt.Println("This is Jasprox.")
-	log.Fatal(http.ListenAndServe(":80", http.HandlerFunc(jasHandler)))
+	proxyHandler := http.HandlerFunc(serveProxy)
+	log.Fatal(http.ListenAndServe(":80", proxyHandler))
 }
